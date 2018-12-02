05e13a3bf240d79034b38fde40e3c01e574fb300
Daniel <d.willian_sc@hotmail.com>
2018-12-02 00:53:43 -0200

    Add way to get all ids from trait

--- Trait.h
+++ Trait.h
@@ -9,6 +9,17 @@ public:
 	virtual bool HasChildren() const { return false; }
 	virtual std::list<Trait*> GetChildren() const { return std::list<Trait*>(); }
 
+	virtual std::list<int> GetAllTraitIds() const
+	{
+		std::list<int> result;
+		result.push_back(id);
+		for (Trait* trait : GetChildren())
+		{
+			result.splice(result.end(), trait->GetAllTraitIds());
+		}
+		return result;
+	}
+
 public:
 	int id;
 };
