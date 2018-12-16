9fe8e4347422a9ee43646213c4eb372630f39ffd
Daniel <d.willian_sc@hotmail.com>
2018-12-16 11:56:48 -0200

    Add talent dictionary for lower level talents

---
+++ TalentDictionary.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+class TalentDictEntry
+{
+public:
+	TalentDictEntry(const std::string& inKey,
+			const std::string& inModifier,
+			const int inType,
+			const std::pair<float, float>& inValues)
+		: key(inKey), modifier(inModifier), type(inType), values(inValues)
+	{
+	}
+
+public:
+	std::string key;
+	std::string modifier;
+	int type;
+	std::pair<float, float> values;
+};
+
+class TalentDictionary
+{
+public:
+	TalentDictionary();
+
+	TalentDictEntry GetDictEntry(const std::string& key)
+	{
+		return *std::find_if(entries.begin(), entries.end(), [&key](const auto e) { return e.key == key; });
+	}
+
+public:
+	static const int BASE = 0;
+	static const int MODIFIER = 1;
+
+public:
+	std::vector<TalentDictEntry> entries;
+	int level;
+};
+
