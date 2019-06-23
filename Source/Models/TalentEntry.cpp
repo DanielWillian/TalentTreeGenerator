08933d8235d26c1010148d46bc3274477cb825d2
Daniel <d.willian_sc@hotmail.com>
2019-06-23 20:55:43 -0300

    Remove unnecessary zeros from talent print

--- Source/Models/TalentEntry.cpp
+++ Source/Models/TalentEntry.cpp
@@ -6,7 +6,8 @@ std::string TalentEntry::toString() const
 	std::string out = "( ";
 	out += property->toString();
 	out += ", " + modifier;
-	out += ", " + std::to_string(value);
+	const std::string valueString = std::to_string(value);
+	out += ", " + valueString.substr(0, valueString.find(".") + 3);
 	out += " )";
 	return out;
 }
